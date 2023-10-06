6557caec77a347a925ef4d4d971fbb1dce5e906b
Kurt Riecken <kurt.riecken@gmail.com>
2023-10-06 07:02:56 -0400

    displayed forecast data to page

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,14 +10,27 @@ $(function () {
             $('#today_temp').html(`Temperature: ${data.main.temp}&deg&nbspF`);
             $('#today_hum').html(`Humidity: ${data.main.humidity}`);
             $('#today_wnd_spd').html(`Wind Speed: ${data.wind.speed}&nbspMPH`);
-            let dateInMS = data.dt*1000;
+            let dateInMS = data.dt * 1000;
+            console.log(dayjs(dateInMS).format('MMM DD, YYYY @ hh:mma'));
+            console.log("+__+_+_+_+_+_++_+_++___+_");
             let currDay = dayjs(dateInMS).format('MMM DD, YYYY');
-            console.log(currDay);
             $('#today_date').text(currDay);
             $('#today_image').html(sunIcon);
             $('#city_name').text(data.name);
-            
+
         });
+
+    fetch('https://api.openweathermap.org/data/2.5/forecast?lat=35.227&lon=-80.8431&units=imperial&appid=fac80ac7de064233ac17d030d9e1eb4f', {})
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            for(let i=7, j=1; i<data.list.length; i+=8, j++) {
+                let newDateInMS = data.list[i].dt * 1000;
+                console.log(dayjs(newDateInMS).format('MMM DD, YYYY @ hh:mma'));
+                $(`#day_${j}`).children('div').children("p.temp").html(data.list[i].main.temp);
+            }
+        })
 
 
 
@@ -25,7 +38,7 @@ $(function () {
     // Initialization of page
     function init() {
 
-    }
+        }
 
 
 
