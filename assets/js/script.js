b6ccf60528d117cc4540f2a7ceadda850e026638
Kurt Riecken <kurt.riecken@gmail.com>
2023-10-06 20:59:20 -0400

    added event listener

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,37 @@
 $(function () {
     let sunIcon = "&#127774";
+    let newCity = '';
+    // TODO: put in weather codes to add tooltip for images
 
+    function readCityInput() {
+        console.log("got through the click");
+        newCity = $('#city_form').find('input').value.trim();
+        if (newCity) {
+            init();
+        }
+        else {
+            alert('Please enter a city');
+            return;
+        }
+    }
 
+    function getLatLon() {
+    fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${newCity}&appid=fac80ac7de064233ac17d030d9e1eb4f`, {})
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data);
+            console.log("AEOIUAETOIAHET");
+        })
+    }
     // Today's weather
     fetch('https://api.openweathermap.org/data/2.5/weather?lat=35.2271&lon=-80.8431&units=imperial&appid=fac80ac7de064233ac17d030d9e1eb4f', {})
         .then(function (response) {
             return response.json();
         })
         .then(function (data) {
-            $('#today_temp').html(`Temperature: ${data.main.temp}&deg&nbspF`);
+            $('#today_temp').html(`Temperature: ${Math.round(data.main.temp)}&deg&nbspF`);
             $('#today_hum').html(`Humidity: ${data.main.humidity}`);
             $('#today_wnd_spd').html(`Wind Speed: ${data.wind.speed}&nbspMPH`);
             let dateInMS = data.dt * 1000;
@@ -31,9 +54,9 @@ $(function () {
             return response.json();
         })
         .then(function (data) {
-            for(let i=7, j=1; i<data.list.length; i+=8, j++) {
+            for (let i = 7, j = 1; i < data.list.length; i += 8, j++) {
                 let headEle = $(`#day_${j}`);
-                
+
                 // Sets day of the week (excepting the first one, which will always display "Tomorrow")
                 if (j != 1) {
                     let newDateInMS = data.list[i].dt * 1000;
@@ -46,26 +69,31 @@ $(function () {
                 headEle.children('div').children("img").attr("src", `http://openweathermap.org/img/w/${imgCode}.png`);
 
                 // Sets temperature for each day
-                let dayTemp = data.list[i].main.temp;
+                let dayTemp = Math.round(data.list[i].main.temp);
                 headEle.children('div').children("p.temp").html(`${dayTemp}&deg&nbspF`);
 
                 // Sets humidiity
 
-                
+
                 // Sets wind speed
             }
         })
 
 
-        http://openweathermap.org/img/w/02n.png
+    // http://openweathermap.org/img/w/02n.png
 
     // Initialization of page
     function init() {
 
-        }
+    }
 
 
-
+    // Event handlers
+    $('#city_button').on("click", function(event) {
+        event.preventDefault();
+        console.log("got through the click");
+        newCity = $('#city_form').find('input').value.trim();
+    });
 
 });
 
